1c32b77c18fe521be3eb22dcfd51a0d0c1db864d
Baridoo Saro <sarobaridoo@gmail.com>
2024-02-27 20:53:45 +0000

    fixed

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,6 @@
 #ifndef BINARY_TREE
 #define BINARY_TREE
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -24,6 +25,9 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 
 typedef struct binary_tree_s heap_t;
+
+/* binary-tree-print */
+void binary_tree_print(const binary_tree_t *tree);
 
 /* DECLARATIONS */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
